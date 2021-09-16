8e08be14531872b6918769ba2397eb8892a4949b
Beksultan50040 <Beksultan.melisov@gmail.com>
2021-09-16 16:08:09 +0600

    FINAL

--- src/main/java/dar/internship/demo/feign/MailFeign.java
+++ src/main/java/dar/internship/demo/feign/MailFeign.java
@@ -3,6 +3,7 @@ package dar.internship.demo.feign;
 import dar.internship.demo.models.dto.MailDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
